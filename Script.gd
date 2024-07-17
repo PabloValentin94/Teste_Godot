b99e661e791a6553acc746ff7d5d6c73e55b9a85
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2024-07-17 05:51:48 -0300

    Fazendo testes relacionados à movimentação de um elemento na interface de usuário.

--- Script.gd
+++ Script.gd
@@ -1,11 +1,43 @@
+# Assim como no Python, os pontos de quebra do GDScript são
+# interpretados através da indentação do código, e não de um
+# caractere especial (Ponto e vírgula, por exemplo.).
+
 extends Sprite2D
 
-# Called when the node enters the scene tree for the first time.
+# O @export faz com que uma variável fique acessível dentro da aba
+# Inspector/Inspetor do elemento ao qual o script está atrelado.
+# Clique no elemento relacionado ao script (No caso atual, é o
+# elemento Image.) e abra a aba Inspector/Inspetor deste elemento
+# (Geralmente, encontra-se na parte direita da interface do Godot.).
+
+# A variável a seguir, pertence ao escopo global do script.
+
+@export var movement_speed = 250
+
+# Função executada assim que o script é carregado.
+
 func _ready() -> void:
 	
-	print("Hello, World!")
+	print("Hello, World!\n")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+# Função executada a cada atualização de quadro (FPS)
+# da interface de usuário.
+
+func _process(delta: float) -> void:
 	
-	pass
+	# O parâmetro delta, contém o valor de tempo que o quadro atual
+	# leva, no intervalo de um segundo (FPS), para ser executado.
+	
+	print(delta)
+	
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Especificando o eixo que sofrerá mudança:
+	
+	# position.x += input.x * (movement_speed * delta)
+	
+	# position.y += input.y * (movement_speed * delta)
+	
+	# Não especificando o eixo que sofrerá mudança (Ambos mudam.):
+	
+	position += input * (movement_speed * delta)
